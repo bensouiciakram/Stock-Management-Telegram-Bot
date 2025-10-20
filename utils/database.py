d2d2473d48d472c74b8714dad217bf16838fd486
bensouici akram <bensouiciakram@gmail.com>
2025-10-20 19:25:40 +0100

    correcting list_cmd in requestdbservice

--- utils/database.py
+++ utils/database.py
@@ -108,14 +108,14 @@ class RequestDbService(BaseDbService):
     def __init__(self,table_name:str):
         super().__init__(table_name=table_name)
 
-    async def list():
+    async def list(self):
         """Return all requests with related admin and nut names."""
         async with aiosqlite.connect(DB_NAME) as db:
             cursor = await db.execute("""
                 SELECT r.id, a.name AS admin, n.name AS nut, r.packages, r.credit_paid, r.description
-                FROM requests r
-                JOIN admins a ON r.admin_id = a.id
-                JOIN nuts n ON r.nut_id = n.id
+                FROM request r
+                JOIN admin a ON r.admin_id = a.id
+                JOIN nut n ON r.nut_id = n.id
             """)
             return await cursor.fetchall()
 
